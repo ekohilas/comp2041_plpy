a111d9c61f3826c0920157d23b0dcbbc5f303644
ekohilas <ekohilas@users.noreply.github.com>
2016-09-30 14:21:39 +1000

    Started and completed Subset 0-1 using regex

--- plpy.pl
+++ plpy.pl
@@ -1,29 +1,71 @@
 #!/usr/bin/perl -w
+use strict;
 
-# written by andrewt@cse.unsw.edu.au September 2016
-# as a starting point for COMP2041/9041 assignment 
-# http://cgi.cse.unsw.edu.au/~cs2041/assignments/plpy/
-# Replace this comment with your own header comment
+# written by Evangelos Kohilas z5114986 
 
-while ($line = <>) {
+while (my $line = <>) {
     if ($line =~ /^#!/ && $. == 1) {
-    
         # translate #! line 
-        
-        print "#!/usr/local/bin/python3.5 -u";
+        print "#!/usr/local/bin/python3.5 -u\n";
+
     } elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
-    
         # Blank & comment lines can be passed unchanged
-        
         print $line;
+
     } elsif ($line =~ /^\s*print\s*"(.*)\\n"[\s;]*$/) {
         # Python's print adds a new-line character by default
         # so we need to delete it from the Perl print statement
-        
-        print "print(\"$1\")\n";
+        my $match = $1;
+
+        # check if we're only printing 1 variable
+        # print it if so 
+        if ($match =~ /\$([A-Za-z_]\w*)\s*/){
+            print "print($1)\n";
+        }
+        else{
+            # otherwise print string
+            print "print(\"$match\")\n";
+        }
+
+    } elsif ($line =~ /^\s*print\s*([^;]*)\s*[\s;]*$/) {
+        print "print(";
+        my @parts = split(",", $1);
+        for (my $i=0; $i < scalar @parts; $i++){
+            my $part = $parts[$i];
+            #replace all variable names
+            if ($part =~ s/\$([A-Za-z_]\w*)/$1/g){
+                #not the last or second last
+                if ($i < scalar @parts - 2){
+                    print "$part, ";
+                }
+                else{
+                    print $part;
+                }
+            }
+            elsif ($part =~ /"\"\\n\""/){
+                last;
+            }
+        }
+        print ")\n";
+        # Convert prints with multiple arguments
+
+    } elsif ($line =~ /^\s*print\s*"(.*)"[\s;]*$/) {
+        # Convert statements without newline
+        print "print(\"$1\", end='')\n";
+
+    } elsif ($line =~ /^\s*\$([A-Za-z_]\w*)\s*\=\s*([^;]*)[\s;]*$/) {
+        # Search for proper scalar variable names and convert
+        my $first_var = $1;
+        my $rest_of_line = $2;
+        $rest_of_line =~ s/\$([A-Za-z_]\w*)/$1/g;
+        print "$first_var = $rest_of_line\n";
+
+    } elsif ($line =~ /^\s*$/){
+        #keep new lines the same
+        print "\n";
+
     } else {
         # Lines we can't translate are turned into comments
-        
         print "#$line\n";
     }
 }
